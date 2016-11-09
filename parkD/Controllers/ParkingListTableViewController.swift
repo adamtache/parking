8b27b80b987ffef03eb739c1626223faf69364e9
Adam Tache <at201@duke.edu>
2016-11-08 22:12:56 -0500

    Organizing into groups: controllers, layouts, models, etc.

---
+++ parkD/Controllers/ParkingListTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ParkingListTableViewController.swift
+//  parkD
+//
+//  Created by Adam on 11/8/16.
+//  Copyright © 2016 ece590. All rights reserved.
+//
+
+import UIKit
+
+class ParkingListTableViewController: UITableViewController{
+    
+    var items: [ParkingLot] = []
+    
+}
